92245a667d2debf2d1fa584fb429634134a6c76a
Joan Serra <122832864+jonysky2023@users.noreply.github.com>
2023-07-26 11:13:00 +0200

    Update User_experience.py

--- python/User_experience.py
+++ python/User_experience.py
@@ -6,8 +6,8 @@ url = "https://west-eu.api.flexxanalyzer.com/api/v4/user-experiences?perPage=10&
 payload={}
 headers = {
   'Accept': 'application/json',
-  'customer-id': '103670da-f3b8-4d51-95dd-49596b9ddf1a',
-  'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXN0b21lcnMiOlsiMTAzNjcwZGEtZjNiOC00ZDUxLTk1ZGQtNDk1OTZiOWRkZjFhIl0sIm5hbWUiOiJhZ2VudCIsInBlcm1pc3Npb25zIjpbXSwiaWF0IjoxNjkwMTkwMDY3LCJleHAiOjE2OTAzNjI4Njd9._ZY5IqK78TsGoHqiq5HMUH3uKX_Aemin9A4s_kcxWz0'
+  'customer-id': '',
+  'Authorization': ''
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
