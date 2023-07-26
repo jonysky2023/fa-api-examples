9e2f554d381c2e063f53abeafd8a48345d0f8b00
Joan Serra <122832864+jonysky2023@users.noreply.github.com>
2023-07-26 11:15:06 +0200

    Update User_experience.py

--- python/User_experience.py
+++ python/User_experience.py
@@ -1,13 +1,17 @@
 import requests
 import pprint
 
+# Define las variables para customer-id y Authorization
+customer_id = '<customer id>'
+bearer_token = '<Bearer Token>'
+
 url = "https://west-eu.api.flexxanalyzer.com/api/v4/user-experiences?perPage=10&page=1"
 
-payload={}
+payload = {}
 headers = {
-  'Accept': 'application/json',
-  'customer-id': '',
-  'Authorization': ''
+    'Accept': 'application/json',
+    'customer-id': customer_id,
+    'Authorization': f'Bearer {bearer_token}'
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
